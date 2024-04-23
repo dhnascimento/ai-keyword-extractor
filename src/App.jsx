1d8704ac38736820cd27621a689a46d84cf7e990
dhnascimento <danielhn@gmail.com>
2024-04-23 18:18:02 -0400

    set states and open ai request

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,43 @@ import { Container, Box } from '@chakra-ui/react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [keywords, setKeywords] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  const extractKeywords = (text) => {
-    console.log(text);
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: 'gpt-3.5-turbo-instruct',
+        prompt: 'Extract keywords from this text. Make the first letter of each word uppercase and separate with commas.\n\n' + text + '',
+        temperature: 0.5,
+        max_tokens: 60,
+        frequency_penalty: 0.8
+      })
+    }
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+    setKeywords(data);
+    setLoading(false);
+  }
+
+  const closeModal = () => {
+    setIsOpen(false);
   }
 
   return (
@@ -18,6 +49,7 @@ function App() {
         <TextInput extractKeywords={extractKeywords} />
         <Footer />
       </Container>
+      <KeywordsModal keywords={keywords} loading={loading} isOpen={isOpen} closeModal={closeModal} />
     </Box>
   );
 }
